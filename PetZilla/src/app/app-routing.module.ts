1105e55f85dcecacc206a90d9123e269c4d9c11e
Nikhil Vallore <Nikhil_Vallore@epam.com>
2019-09-03 11:31:10 +0530

    featureModuleRoutingAndSharedAlertComponent

--- PetZilla/src/app/app-routing.module.ts
+++ PetZilla/src/app/app-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './components/login/login.module#LoginModule'
+  },
+  {
+    path: 'dashboard',
+    loadChildren: './components/dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: 'ourmedia',
+    loadChildren: './components/our-media/our-media.module#OurMediaModule'
+  },
+  {
+    path: 'infotab',
+    loadChildren: './components/info-tab/info-tab.module#InfoTabModule'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
