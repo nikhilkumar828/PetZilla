f03ea43d8c8e5b738bc6f3cdd617acbb8c115f4f
Nikhil Vallore <Nikhil_Vallore@epam.com>
2019-09-03 10:52:08 +0530

    featureModules

---
+++ PetZilla/src/app/components/our-media/our-media.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OurMediaRoutingModule } from './our-media-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OurMediaRoutingModule
+  ]
+})
+export class OurMediaModule { }
