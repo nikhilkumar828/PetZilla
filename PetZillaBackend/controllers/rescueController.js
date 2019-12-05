c0fced875a7a687625431234ef7df3a4996d9efd
Nikhil V <nikhilvallore828@gmail.com>
2019-12-05 22:22:31 +0530

    Rescue Service Fix

--- PetZillaBackend/controllers/rescueController.js
+++ PetZillaBackend/controllers/rescueController.js
@@ -75,7 +75,7 @@ router.post('/',
         author: req.body.author,
         createdDate: new Date(),
         mobileNo: parseInt(req.body.mobileNo),
-        authorId: req.body.mailId
+        // authorId: req.body.mailId
     });
     postlist.addList(newPost,(err, list) => {
         if(err) {
